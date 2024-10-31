8f19499ce5fb554e10a9961b6c6b6c5f30606815
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-10-31 16:03:35 +0000

    make warnings into errors in strict mode

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! Quadrature
+#![cfg_attr(feature = "strict", deny(warnings), deny(unused_crate_dependencies))]
+#![warn(missing_docs)]
+
 pub mod duffy;
 pub mod simplex_rule_definitions;
 pub mod simplex_rules;
